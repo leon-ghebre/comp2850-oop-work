a071c6276483c2d4f31564e2d4e973c5118f6dbe
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-04 17:04:42 +0100

    task2_5_2

--- tasks/task2_5_1/app/src/main/kotlin/org/example/App.kt
+++ tasks/task2_5_1/app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,10 @@
 fun main() {
-    val mynum = 28
-    println(mynum)
-    mynum = 32
-    println(mynum)
+    val myAge = 29u
+    val universeAge = 13_800_000_000L
+    val status = 'M'
+    val name = "Sarah"
+    val height = 1.78f
+    val root2 = Math.sqrt(2.0)
+
 }
 
