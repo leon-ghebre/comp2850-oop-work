870cdc3ce381cc3ad228663d91a4847e83f87d24
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-04 16:55:22 +0100

    yes

--- tasks/task2_5_1/app/build.gradle.kts
+++ tasks/task2_5_1/app/build.gradle.kts
@@ -40,7 +40,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+    mainClass.set("AppKt")
 }
 
 tasks.named<Test>("test") {
--- tasks/task2_5_1/app/src/main/kotlin/org/example/App.kt
+++ tasks/task2_5_1/app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,5 @@
 fun main() {
     var mynum = 28
     println(mynum)
-    mynum = 5
-    println(mynum)
 }
 
