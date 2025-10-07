3249e0a7f07e57fe754dff6eddb6c920d22da221
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-07 14:39:13 +0100

    saving Args.kt

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,6 +1,13 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    println(args[0])
+    if (args.size != 2) {
+        println("Error: Two arguments are required!")
+        exitProcess(1)
+    }
+    println(args[0])Í
     println(args[1])
 }
+
+
