19b1678215d0e923d6beb8275da85c129b9b721f
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-11 17:41:50 +0100

    oddsum completed

--- tasks/task4_5/OddSum.kt
+++ tasks/task4_5/OddSum.kt
@@ -1,1 +1,10 @@
 // Task 4.5: summing odd integers with a for loop
+fun main() {
+    println("Please enter the upper limit of the for loop: ")
+    var upper_limit = readln().toLong()
+    var sum: Long = 0
+    for (n in 1..upper_limit step 2) {
+        sum += n
+    }
+    println("Sum: $sum")
+}
