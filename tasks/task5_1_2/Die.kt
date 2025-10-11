f277136df7b9a11888c862f12c9fcf73f7d606dd
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-11 19:19:51 +0100

    die game done

--- tasks/task5_1_2/Die.kt
+++ tasks/task5_1_2/Die.kt
@@ -1,1 +1,23 @@
 // Task 5.1.2: die rolling simulation
+import kotlin.random.Random
+fun rollDie(sides: Int) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled $result")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
+fun main() {
+    var die_sides = readInt("Specify the number of die sides: ")
+    rollDie(die_sides)
+}
+
+fun readInt(first: String): Int {
+    println(first)
+    var input = readln().toInt()
+    return input
+}
