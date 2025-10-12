18e13e178244e8df0bdae463e849f7a9b3b75c7c
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-12 20:11:56 +0100

    grades.kt done

--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,1 +1,21 @@
 // Task 5.2.2: conversion of marks into grades, using a function
+import kotlin.system.exitProcess
+
+fun grade(mark: Int) = when (mark) {
+    in 0..39   -> "Fail"
+    in 40..69  -> "Pass"
+    in 70..100 -> "Distinction"
+    else       -> "?"
+}
+
+fun main(args: Array<String>) {
+    if (args.size < 1) {
+        print("Please provide some arguments! ")
+        exitProcess(1)
+    }
+    for (arg in args){
+        var mark = arg.toInt()
+        var grade = grade(mark)
+        println("$mark is a $grade")
+    }
+}
