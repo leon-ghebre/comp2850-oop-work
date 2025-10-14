3c14fae90db8ad93274b8de9936344f91fd4b326
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-15 00:55:05 +0100

    dice.kt done

--- tasks/task5_3_2/Dice.kt
+++ tasks/task5_3_2/Dice.kt
@@ -1,1 +1,22 @@
 // Task 5.3.2: dice rolling simulation
+import kotlin.random.Random
+
+fun rollDice(sides: Int=6, dice: Int=1){
+    var total = 0
+    for (i in 1..dice){
+        if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+            println("Rolling a d$sides...")
+            val result = Random.nextInt(1, sides + 1)
+            total += result
+            println("Total was $total")
+        }
+        else {
+            println("Error: cannot have a $sides-sided die")
+        }
+    }
+}
+
+fun main() {
+    rollDice(dice = 100)
+
+}
