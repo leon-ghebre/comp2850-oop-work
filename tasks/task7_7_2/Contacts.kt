b85f18aa8510372bd7a1115257bdc012015cbfb4
Leon Ghebre <leon.ghebre@nhs.net>
2025-10-20 17:15:55 +0100

    contacts.kt finished

--- tasks/task7_7_2/Contacts.kt
+++ tasks/task7_7_2/Contacts.kt
@@ -5,7 +5,8 @@ fun readData() = buildList {
         println("Please enter a floating point value")
         val input = readln()
         if (input.isBlank()) {
-            var loopOn = false
+            loopOn = false
+            break
         } 
         val number = input.toDoubleOrNull()
         if (number!=null){
@@ -18,29 +19,34 @@ fun readData() = buildList {
 
 fun median(list: List<Double>): Double{
     val sorted = list.sorted()
-    val size = list.size()
+    val size = list.size
     if (size % 2 == 0) {
         val medianPosition = size / 2
+        return sorted[medianPosition - 1]
+    } else if (size == 1){
+        val medianPosition = 0
         return sorted[medianPosition]
     } else {
         val medianPosition = (size + 1) / 2
-        return sorted[medianPosition]
+        return sorted[medianPosition - 1]
     }
 }
 
-fun stats(list: List<Double>): List{
+fun stats(list: List<Double>):List<Double>{
     val sorted = list.sorted()
-    val size = list.size()
+    val size = list.size
     val median = median(list)
     val min = list.min()
     val max = list.max()
     val sum = list.sum()
     val average = list.average()
-    return (median, min, max, sum, average)
+    val returnList = listOf(median, min, max, sum, average)
+    return returnList
     
 }
 
 fun main(){
     val list = readData()
-    stats = stats(list)
+    val stats = stats(list)
+    println(stats)
 }
